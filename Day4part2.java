792b9223b73c9e3cbfb16da51a63c8ffe290bedb
Secondaire <secondaire@MacBook-Pro-de-Cemal.local>
2022-12-05 22:30:03 +0100

    day 4 5

---
+++ Day4part2.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+public class Day4part2 {
+    public static void main(String[] args) throws Exception {
+        // Get inputs
+        URL url = new URL("https://adventofcode.com/2022/day/4/input");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+        conn.setRequestProperty("Cookie","_04");
+        conn.setRequestProperty("Content-Type","application/json");
+        conn.setRequestMethod("GET");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+
+        String output;
+
+        StringBuffer response = new StringBuffer();
+
+
+        int sum = 0;
+
+        while ((output = in.readLine()) != null) {
+            System.out.println(output);
+            String[] values = output.split(",");
+            String[] range1 = values[0].split("-");
+            String[] range2 = values[1].split("-");
+
+            int range11 = Integer.parseInt(range1[0]);
+            int range12 = Integer.parseInt(range1[1]);
+
+            int range21 = Integer.parseInt(range2[0]);
+            int range22 = Integer.parseInt(range2[1]);
+
+
+            /*
+            range11 == range21 || range12 == range22 ||
+
+             */
+            if( range11 == range21 || range12 == range22 ||
+                    (range11 > range21 && range12 < range22) ||
+                    (range21 > range11 && range22 < range12) ||
+                    (range21 >= range11 && range21 <= range12) ||
+                    (range22 >= range11 && range22 <= range12) ||
+                    (range11 >= range21 && range11 <= range22) ||
+                    (range12 >= range21 && range12 <= range22)
+            ){
+                sum++;
+                System.out.println("ok");
+            }
+
+        }
+// > 441
+        System.out.println(sum);
+
+        //in.close();
+    }
+}
---
+++ Day5part2.java
@@ -0,0 +1,126 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class Day5part2 {
+    public static void main(String[] args) throws Exception {
+        // Get inputs
+        URL url = new URL("https://adventofcode.com/2022/day/5/input");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+        conn.setRequestProperty("Cookie","");
+        conn.setRequestProperty("Content-Type","application/json");
+        conn.setRequestMethod("GET");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+        StringBuffer response = new StringBuffer();
+
+        LinkedList<String> cargo = new LinkedList<>();
+        for(int i=0; i<8; i++) {
+            cargo.push(in.readLine());
+        }
+
+        List<LinkedList<Character>> colones = new ArrayList<>();
+        for(int i=0; i<9; i++) {
+            colones.add(new LinkedList<>());
+        }
+
+        for(int x=0; x<8; x++) {
+            String lastline = cargo.pop();
+            for(int i = 0; i < 9; i++){
+
+
+                char lastChar = lastline.charAt((i*4)+1);
+
+               if(lastChar >= 'A' && lastChar <= 'Z')
+                    colones.get(i).push(lastChar);
+            }
+        }
+
+
+        // ligne inutile
+        in.readLine();
+        in.readLine();
+
+        String output;
+        while ((output = in.readLine()) != null) {
+            String deplacement = output.substring(5);
+
+            int fin = deplacement.indexOf(' ');
+            String nbADeplacer = deplacement.substring(0, fin);
+            int nb = Integer.parseInt(nbADeplacer);
+
+            char coloneAPop = deplacement.charAt(deplacement.length() - 6);
+            int from = Integer.parseInt(String.valueOf(coloneAPop)) - 1;
+
+            char coloneADeposer = deplacement.charAt(deplacement.length() - 1);
+            int to = Integer.parseInt(String.valueOf(coloneADeposer)) - 1;
+
+            List<Character> aDeposerEnBloc = new ArrayList<>();
+            while(nb > 0){
+                nb--;
+                aDeposerEnBloc.add(colones.get(from).pop());
+            }
+
+            Collections.reverse(aDeposerEnBloc);
+
+            aDeposerEnBloc.forEach(c -> colones.get(to).push(c));
+
+        }
+
+        /*
+        R
+N
+Z
+L
+F
+Z
+S
+J
+H
+         */
+
+
+        for(int k=0; k<9; k++) {
+            LinkedList<Character> colone = colones.get(k);
+            System.out.println(colone.pop());
+        }
+        /*
+
+move 6 from 1 to 7
+move 2 from 2 to 4
+move 2 from 7 to 4
+move 6 from 4 to 3
+move 1 from 5 to 1
+move 3 from 8 to 3
+move 15 from 3 to 4
+move 6 from 5 to 9
+move 14 from 4 to 2
+move 3 from 2 to 7
+move 1 from 2 to 7
+move 9 from 9 to 1
+move 3 from 2 to 1
+
+         */
+        /**
+         * [S]                 [T] [Q]
+         * [L]             [B] [M] [P]     [T]
+         * [F]     [S]     [Z] [N] [S]     [R]
+         * [Z] [R] [N]     [R] [D] [F]     [V]
+         * [D] [Z] [H] [J] [W] [G] [W]     [G]
+         * [B] [M] [C] [F] [H] [Z] [N] [R] [L]
+         * [R] [B] [L] [C] [G] [J] [L] [Z] [C]
+         * [H] [T] [Z] [S] [P] [V] [G] [M] [M]
+         *  1   2   3   4   5   6   7   8   9
+         */
+
+        in.close();
+    }
+}
