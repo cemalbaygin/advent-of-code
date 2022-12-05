18e8f0cad1e3cf4dfaa1c6d3382866e3714f2c0e
Secondaire <secondaire@MacBook-Pro-de-Cemal.local>
2022-12-05 22:31:01 +0100

    refacto of death

--- Day5part2.java
+++ Day5part2.java
@@ -75,51 +75,11 @@ public class Day5part2 {
 
         }
 
-        /*
-        R
-N
-Z
-L
-F
-Z
-S
-J
-H
-         */
-
 
         for(int k=0; k<9; k++) {
             LinkedList<Character> colone = colones.get(k);
             System.out.println(colone.pop());
         }
-        /*
-
-move 6 from 1 to 7
-move 2 from 2 to 4
-move 2 from 7 to 4
-move 6 from 4 to 3
-move 1 from 5 to 1
-move 3 from 8 to 3
-move 15 from 3 to 4
-move 6 from 5 to 9
-move 14 from 4 to 2
-move 3 from 2 to 7
-move 1 from 2 to 7
-move 9 from 9 to 1
-move 3 from 2 to 1
-
-         */
-        /**
-         * [S]                 [T] [Q]
-         * [L]             [B] [M] [P]     [T]
-         * [F]     [S]     [Z] [N] [S]     [R]
-         * [Z] [R] [N]     [R] [D] [F]     [V]
-         * [D] [Z] [H] [J] [W] [G] [W]     [G]
-         * [B] [M] [C] [F] [H] [Z] [N] [R] [L]
-         * [R] [B] [L] [C] [G] [J] [L] [Z] [C]
-         * [H] [T] [Z] [S] [P] [V] [G] [M] [M]
-         *  1   2   3   4   5   6   7   8   9
-         */
 
         in.close();
     }
